9a7d1cb609437aeb6d0280a1c09bd1a723d1837b
javier S <javier.san.mail@gmail.com>
2020-04-01 18:19:37 +0200

    update

--- 5-ConcatenativeInhereitancePattern.js
+++ 5-ConcatenativeInhereitancePattern.js
@@ -4,6 +4,7 @@ const person = {
         return `Hello, my name is ${this.name} and I a ${this.gender}`;
     }
 };
-const mario = Object.assign({}, person, { name: 'Mario' });
-const msg = george.hello();
+const mario = Object.assign({}, person, { name: 'Mario', shout: function () { return alert("aaaaaahhh!!"); } });
+const msg = mario.hello();
 console.log(msg); // Hello, my name is Mario and I a male
+console.log(mario.shout())
