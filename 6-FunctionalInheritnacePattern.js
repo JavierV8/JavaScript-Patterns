cf63fb46aacac101ae0fbe939d59c6b294528333
javier S <javier.san.mail@gmail.com>
2020-04-03 18:14:52 +0200

    function inhereitance

---
+++ 6-FunctionalInheritnacePattern.js
@@ -0,0 +1,36 @@
+const ridingBike = (state) => ({
+    ride: (bike) => {
+        console.log(`${state.name} is riding his ${bike}!`);
+        state.energy--;
+    }
+})
+const drivingCar = (state) => ({
+    drive: (car) => {
+        console.log(`${state.name} is driving a ${car}`);
+        state.oil--;
+    }
+})
+
+const cyclist = (name) => {
+    let state = {
+        name,
+        energy: 100
+    }
+    return Object.assign(state, ridingBike(state));
+}
+
+const pilot = (name) => {
+    let state = {
+        name,
+        oil: 100
+    }
+    return Object.assign(state, canCast(state));
+}
+
+const indurain = cyclist('Miguel Indurain')
+indurain.ride('pinarello dogma');  // Miguel Indurain is riding his pinarello dogma
+console.log(indurain.energy)      // 99
+
+const schumacher = pilot('michael schumacher')
+schumacher.drive('ferrari');      // michael schumacher is driving a ferrari
+console.log(schumacher.oil)       // 99
