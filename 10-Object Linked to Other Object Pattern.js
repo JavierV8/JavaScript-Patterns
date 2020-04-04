0f6868189dde6ac49d3a0aa2b3e0a980802e1c36
javier S <javier.san.mail@gmail.com>
2020-04-04 18:29:10 +0200

    object.crate pattern

--- 10-Object Linked to Other Object Pattern.js
+++ 10-Object Linked to Other Object Pattern.js
@@ -1,25 +1,23 @@
 const Vehicle = {
-    engeen: true,
     init: function (brand, model, year) {
         this.brand = brand;
         this.model = model;
         this.year = year;
     },
+    create: function (values) {
+        var instance = Object.create(this);
+        Object.keys(values).forEach(function (key) {
+            instance[key] = values[key];
+        });
+        return instance;
+    },
     start: function () {
         return "VROOM!"
     },
     VehicleInfo: function () {
         return `The brand, model, and year are ${this.brand} ${this.model} ${this.year}`;
     }
-}/*
-Vehicle.prototype = {
-    start: function () {
-        return "VROOM!"
-    },
-    VehicleInfo: function () {
-        return `The brand, model, and year are ${this.brand} ${this.model} ${this.year}`;
-    }
-};*/
+}
 
 var car = Vehicle.create({
     numWheels: 4,
@@ -34,46 +32,13 @@ var Motorcycle = Vehicle.create({
     }
 })
 
-var MyCar = Car.create("volkswagen", "polo", "2018", 4);
+var MyCar = car.create({});
+MyCar.init("volkswagen", "polo", "2018")
 console.log(MyCar.VehicleInfo())
 console.log(MyCar.startRadio())
 
-var MyMotorcycle = new Motorcycle("Honda", "CBR600RR", "2019", 2);
+var MyMotorcycle = Motorcycle.create({});
+MyMotorcycle.init("Honda", "CBR600RR", "2019")
 console.log(MyMotorcycle.VehicleInfo())
 console.log(MyMotorcycle.makeWheelie())
-
-
-
-
-//-----------------------------------------------------------------
-
-const human = {
-    species: "human",
-    create: function (values) {
-        var instance = Object.create(this);
-        Object.keys(values).forEach(function (key) {
-            instance[key] = values[key];
-        });
-        return instance;
-    },
-    saySpecies: function () {
-        console.log(this.species);
-    },
-    sayName: function () {
-        console.log(this.name);
-    }
-};
-
-var musician = human.create({
-    species: "musician",
-    playInstrument: function () {
-        console.log("plays " + this.instrument);
-    }
-});
-var will = musician.create({
-    name: "Will",
-    instrument: "drums"
-});
-will.playInstrument(); // plays drums
-will.sayName(); //will
 
